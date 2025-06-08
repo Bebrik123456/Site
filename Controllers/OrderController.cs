100805a7dafd1a06af86d8c64bcce74c506dcab1
Михатл <mikhailcher2006@gmail.com>
2025-06-08 13:36:01 +0300

    подготовка логики для лучших времен

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿namespace Site.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+public class OrderController : Controller
+{
+    // Отображает форму (GET)
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // Обрабатывает отправку (POST)
+    [HttpPost]
+    public IActionResult Submit(OrderRequest request)
+    {
+        // Здесь логика: сохранение в БД, отправка email и т.д.
+        Console.WriteLine($"Заказ от {request.Name}, тел.: {request.Phone}");
+
+        // Перенаправляем на "Спасибо"
+        return RedirectToAction("ThankYou");
+    }
+
+    public IActionResult ThankYou()
+    {
+        return View();
+    }
+}
---
+++ Models/OrderRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Site.Models;
+
+public class OrderRequest
+{
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public DateTime EventDate { get; set; }
+    public int Guests { get; set; }
+    public string Comments { get; set; }
+}
